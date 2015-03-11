fb315b704d88468e2785fc80345fd7621a6d7a5f
fisker23 <fisker23@hotmail.com>
2015-03-11 14:58:45 +0100

    Entity initialized

--- src/java/rest/ApiResource.java
+++ src/java/rest/ApiResource.java
@@ -5,26 +5,33 @@
  */
 package rest;
 
+import com.google.gson.Gson;
+import entity.Person;
+import java.util.HashMap;
+import java.util.Map;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.UriInfo;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
 
 /**
  * REST Web Service
  *
  * @author Andreas Fisker
  */
+//Ca2Orm
 @Path("api")
 public class ApiResource {
 
     @Context
-    private UriInfo context;
-
+    private Map<Integer,Person> personMap = new HashMap<Integer, Person>(); 
+    Gson gson = new Gson();
     /**
      * Creates a new instance of ApiResource
      */
@@ -41,7 +48,32 @@ public class ApiResource {
         //TODO return proper representation object
         throw new UnsupportedOperationException();
     }
-
+    
+    @GET
+    @Produces("application/json")
+    @Path("/person/complete")
+    public String getAllPjson(){
+        
+        return gson.toJson(personMap.values()); 
+    }
+    @GET
+    @Produces("application/json")
+    @Path("/person/{id}")
+    public String getOnePjson(@PathParam("id") int id){
+        Person p = personMap.get(id);
+        if(!personMap.containsKey(id)){
+            throw new WebApplicationException("No person with the given ID found.", Response.Status.NOT_FOUND);
+        }
+        return gson.toJson(p); 
+    }
+    
+    @GET
+    @Produces("application/json")
+    @Path("/person/contactinfo")
+    public String getAllContactPjson(){
+        
+        return gson.toJson(personMap.values()); 
+    }
     /**
      * PUT method for updating or creating an instance of ApiResource
      * @param content representation for the resource
@@ -51,4 +83,12 @@ public class ApiResource {
     @Consumes("application/json")
     public void putJson(String content) {
     }
+    
+//    @POST
+//    @Consumes("application/json")
+//    @Path("/person")
+//    public Person(String jsonin){
+//        Person P = new Person();
+//        return P;
+//    }
 }
