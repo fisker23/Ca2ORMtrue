99e4b0e6a3643608d84302ac8feafee0508cfedb
fisker23 <fisker23@hotmail.com>
2015-03-12 10:42:30 +0100

    lille update

--- src/java/rest/ApiResource.java
+++ src/java/rest/ApiResource.java
@@ -6,8 +6,13 @@
 package rest;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
 import entity.Person;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.Produces;
@@ -19,6 +24,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
+import org.eclipse.persistence.sessions.Project;
 
 /**
  * REST Web Service
@@ -71,8 +77,28 @@ public class ApiResource {
     @Produces("application/json")
     @Path("/person/contactinfo")
     public String getAllContactPjson(){
-        
-        return gson.toJson(personMap.values()); 
+   String str  = "";
+   
+        for (int i = 0; i < personMap.size(); i++) {
+            str += personMap.get(i).getEmail() + personMap.get(i).getFirstName() + personMap.get(i).getLastName();
+            
+        }
+   return gson.toJson(str); 
+    }
+    @GET
+    @Produces("application/json")
+    @Path("/person/contactinfo/{id}")
+    public String getOneContactPjson(@PathParam("id") int id){
+         
+          Person p = personMap.get(id);
+        if(!personMap.containsKey(id)){
+            throw new WebApplicationException("No person with the given ID found.", Response.Status.NOT_FOUND);
+        }
+        JsonObject jo = new JsonObject();
+        jo.addProperty("firstName", p.getFirstName());
+        jo.addProperty("lastName", p.getLastName());
+        jo.addProperty("email",p.getEmail());
+        return gson.toJson(jo); 
     }
     /**
      * PUT method for updating or creating an instance of ApiResource
@@ -81,14 +107,21 @@ public class ApiResource {
      */
     @PUT
     @Consumes("application/json")
+    @Path("/person")
     public void putJson(String content) {
+       // JsonObject o = new JsonParser().parse(jsonAsString).getAsJsonObect();
+       
     }
     
-//    @POST
-//    @Consumes("application/json")
-//    @Path("/person")
-//    public Person(String jsonin){
-//        Person P = new Person();
-//        return P;
-//    }
+    @POST
+    @Consumes("application/json")
+    @Path("/person")
+    public Person createPerson(String jsonin){
+         java.lang.reflect.Type type = new TypeToken<Collection<Project>>(){}.getType();
+    List<Person> persons = gson.fromJson(jsonin,type);
+        for (Person person : persons) {
+            System.out.println("TEST: "+person.getFirstName());
+        }
+        return persons.get(1);
+    }
 }
